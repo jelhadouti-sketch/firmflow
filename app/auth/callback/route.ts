bc97a5398abfe0f0573607c46110e4b2964671f1
Jamal Elhadouti <jamalelhadouti@macbookpro.home>
2026-03-28 17:25:54 +0100

    fix auth callback URL

---
+++ app/auth/callback/route.ts
@@ -0,0 +1,18 @@
+import { createClient } from '@/lib/supabase/server'
+import { NextRequest, NextResponse } from 'next/server'
+
+export async function GET(req: NextRequest) {
+  const { searchParams } = new URL(req.url)
+  const code = searchParams.get('code')
+  const next = searchParams.get('next') || '/reset-password'
+
+  if (code) {
+    const supabase = await createClient()
+    const { error } = await supabase.auth.exchangeCodeForSession(code)
+    if (!error) {
+      return NextResponse.redirect(new URL(next, req.url))
+    }
+  }
+
+  return NextResponse.redirect(new URL('/forgot-password?error=link-expired', req.url))
+}
